54cf59d56c33e646d6915105cb37b73db223b060
avee <avee@avees-MacBook-Air.local>
2023-03-29 02:47:17 +0600

    urls modified

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.storeView.as_view(), name="store"),
-    path("edit_store", views.editStore.as_view(), name="edit_store"),
-    path("update_store", views.updateStore.as_view(), name="update_store"),
-    path("delete_store", views.deleteStore.as_view(), name="delete_store"),
+    path("details/<int:id>/", views.StoreDetailsView.as_view(), name="store-details"),
 ]
