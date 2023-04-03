1805fe7b5b195785614e7f18be6975a29ee18314
naimulnobel <nobelnaimulislam@gmail.com>
2023-04-03 16:43:25 +0600

    item type curd

--- item/urls.py
+++ item/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("item-type", views.ItemTypeView.as_view(), name="item-type"),
+    path("item-type-detail", views.ItemTypeDetailsView.as_view(), name="item-type-detail"),
     path("", views.ItemView.as_view(), name="item"),
     path("item-detail", views.ItemDetailView.as_view(), name="item-detail"),
 ]
