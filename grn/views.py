54df2e58ffb4b97ad179f7dedfa544b039b67ac4
avee <avee@avees-MacBook-Air.local>
2023-04-06 01:35:08 +0600

    uom added in item model

--- grn/views.py
+++ grn/views.py
@@ -3,7 +3,7 @@ from django.views import View
 from .forms import GrnForms, GrnDetailsForms
 from .models import Grn, GrnDetails
 
-class GRNView(View):
+class GrnView(View):
     form_class = GrnForms
     template_name = "grn.html"
 
@@ -11,3 +11,11 @@ class GRNView(View):
         grn = Grn.objects.all().order_by("-id")
         context = {"grn": grn}
         return render(request, context, self.template_name)
+
+    def post(self, request):
+        payload = request.POST
+        grn_serializer = GrnForms(payload)
+        if grn_serializer.is_valid():
+            grn = grn_serializer.save()
+        else:
+            pass
--- item/models.py
+++ item/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 
+class Uom(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "uom"
+
 class ItemType(models.Model):
     name = models.CharField(max_length=50, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
