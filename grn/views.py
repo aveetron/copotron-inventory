1f3d90f74d21a1c1fcf9e4440824fa19912541e0
naimulnobel <nobelnaimulislam@gmail.com>
2023-04-09 22:49:06 +0600

    grn post

--- grn/views.py
+++ grn/views.py
@@ -4,6 +4,8 @@ from .forms import GrnForms, GrnDetailsForms
 from .models import Grn, GrnDetails
 from item.models import Item
 from store.models import Store
+from django.contrib import messages
+from django.shortcuts import HttpResponseRedirect
 
 
 class GrnView(View):
@@ -21,5 +23,21 @@ class GrnView(View):
         grn_serializer = GrnForms(payload)
         if grn_serializer.is_valid():
             grn = grn_serializer.save()
+            item = payload.getlist('item')
+            quantity = payload.getlist('quantity')
+            zipped = zip(item, quantity)
+            for item, quantity in zipped:
+                data = {'grn': grn.id, 'item': item, 'quantity': quantity}
+
+                grn_details = GrnDetailsForms(data)
+
+                if grn_details.is_valid():
+                    grn_details.save()
+            grn.save()
+            massage = "grn created"
+            messages.success(request, massage)
+            return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
         else:
-            pass
+            massage = grn_serializer.errors
+            messages.warning(request, massage)
+            return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
