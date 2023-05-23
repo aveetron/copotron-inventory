4f7580a292fca56e93338b9ffb8f157ac188c615
avee <avee@avees-MacBook-Air.local>
2023-05-24 00:07:18 +0600

    dashboard added, search bar removed, notification and message removed

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from item.views import DashboardView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("item/", include("item.urls")),
     path("store/", include("store.urls")),
     path("grn/", include("grn.urls")),
+    path("", DashboardView.as_view(), name="dashboard"),
 ]
